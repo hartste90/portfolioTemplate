a16cdee5ba26a1860f341802bb9c53c0c3eb1601
hartste90 <hartste90@gmail.com>
2018-03-26 18:43:46 -0700

    fixing up

---
+++ www/src/socialmediabuttons.js
@@ -0,0 +1,13 @@
+function redirectToLinkedin() {
+	document.location.href = "https://www.linkedin.com/in/steven-hart-65458026";
+}
+
+function redirectToGithub() {
+	document.location.href = "https://github.com/hartste90";
+}
+
+function redirectToEmail() {
+    var link = "mailto:steven.hart282@gmail.com"
+             + "&subject=" + escape("Hi Steve, I've been looking at your portolio!");
+    document.location.href = link;
+}
